36226d9fe0f8e8efeac81eba190638e288c73f40
Leandro Simonato <leandrosimonato@gmail.com>
2021-05-16 12:04:39 -0300

    Projeto inicial

---
+++ src/main/java/com/example/javacompipoca/JavaComPipocaApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacompipoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaComPipocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaComPipocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javacompipoca/JavaComPipocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacompipoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaComPipocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
